d110e5c2f9ade29eafb0370b21405d5bb456bca2
dokinoki <dokinoki@users.noreply.github.com>
2015-08-18 15:18:54 -0700

    Delete pushPackage.php

--- pushPackage.php
+++
@@ -1,9 +0,0 @@
-    <?php
-    
-    require_once('pushPackageClass.php');
-    
-    //Create a push package using the class and the userID
-    $objPushClass = new pushPackageClass($_COOKIE['USER_ID']);
-    
-    //Generate a ZIP file and serve it
-    $objPushClass->serve_push_package();
